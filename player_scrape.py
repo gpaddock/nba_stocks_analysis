540c37b76c2f28ea3010c4e8371d2703b1540975
Augustus Paddock <38698177+gpaddock@users.noreply.github.com>
2024-04-10 16:17:55 -0700

    link to existing scraper package

--- player_scrape.py
+++ player_scrape.py
@@ -1,4 +1,5 @@
 # File for creating the master data containing player defensive statistics
+# https://github.com/vishaalagartha/basketball_reference_scraper/blob/master/API.md ?
 # Author: Gus Paddock
 
 # IMPORTS
